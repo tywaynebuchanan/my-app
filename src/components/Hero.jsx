d4c630143da46a4026bfb4e3bc45e7d3d6d00238
Tywayne <tywaynebuchanan@gmail.com>
2022-05-11 18:24:40 -0500

    Added items to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,8 +12,24 @@ const Hero = () => {
             <p className='text-2xl'>Leotis Programming Company</p>
             <h1 className='py-3 text-5xl md:text-7xl font-bold'>Leotis Works</h1>
             <p className='text-2xl'>This is his brand</p>
-            <button className='py-3 px-6'>Get Started</button>
+            <button className='py-3 px-6 sm:w-[60%] my-4'>Get Started</button>
             </div>
+
+            <div>
+                <img src={bgImg} className = "w-full" alt="Computer" />
+            </div>
+
+            <div className ='absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
+            mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border
+             border-slate-300 rounded-xl text-center shadow-xl'>
+                <p>Data Services</p>
+                <div className='flex justify-between flex-wrap px-4'>
+                    <p className = "flex px-4 py-2"><CloudUploadIcon className = "h-4 text-blue-500"/>Data Services</p>
+                    <p className = "flex px-4 py-2"><CloudUploadIcon className = "h-4 text-blue-500"/>Data Services</p>
+                    <p className = "flex px-4 py-2"><CloudUploadIcon className = "h-4 text-blue-500"/>Data Services</p>
+                    <p className = "flex px-4 py-2"><CloudUploadIcon className = "h-4 text-blue-500"/>Data Services</p>
+                </div>
+            </div>  
            
         </div>
 
